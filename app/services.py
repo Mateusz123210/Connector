bf5922f9102541135d92c0400f3a8b3262a6fc62
Mateusz123210 <mateusz.urbanczyk11@gmail.com>
2024-04-09 19:04:24 +0200

    Done sending and fetching emails

--- app/services.py
+++ app/services.py
@@ -1,28 +1,42 @@
 from database import collection_name
-from models import Conversation
-# from schemas import list_serial
-from bson import ObjectId
+from fastapi import HTTPException
 
 def get_messages(data):
-    conversations = collection_name.find()
-    for doc in conversations:
-        print(doc)
-        print(type(doc))
-    return str(conversations)
-
-    # return {"haha2"}
+    found_conversation = collection_name.find_one({"first_user": data["first_user"], "second_user": data["second_user"]})
+    if found_conversation:
+        list1 = found_conversation["message"]
+        return {"messages": list1}
+    else:
+        found_conversation = collection_name.find_one({"first_user": data["second_user"], "second_user": data["first_user"]})
+        if found_conversation:
+            list1 = found_conversation["message"]
+            return {"messages": list1}
+        else:
+            return {"message": "No messages sent to this receiver yet!"}
 
 def send_message(data):
-    # conversation = {"first_user": "252808@student.pwr.edu.pl",
-    #     "second_user": "mateusz.urbanczyk11@gmail.com",
-    #     "messages_list": [["osfmdomo", "left"]]
-    #     }
-    # # c = Conversation(first_user= "252808@student.pwr.edu.pl",
-    # #     second_user= "mateusz.urbanczyk11@gmail.com",
-    # #     messages_list= [["osfmdomo", "left"]]
-    # #     )
+    if len(data["message"]) == 0:
+        raise HTTPException(status_code=400, detail="You cannot send empty message!")
+    found_conversation = collection_name.find_one({"first_user": data["first_user"], "second_user": data["second_user"]})
+    if found_conversation:
+        list1 = found_conversation["message"]
+        list1.append({data["message"]: "from_first"})
+        filter = { '_id': found_conversation["_id"] }
+        new_values = { "$set": { 'message': list1 } }
+        collection_name.update_one(filter, new_values)
+    else:
+        found_conversation = collection_name.find_one({"first_user": data["second_user"], "second_user": data["first_user"]})
+        if found_conversation:
+            list1 = found_conversation["message"]
+            list1.append({data["message"]: "from_second"})
+            filter = { '_id': found_conversation["_id"] }
+            new_values = { "$set": { 'message': list1 } }
+            collection_name.update_one(filter, new_values)
+        else:
+            insert_data = {"first_user": data["first_user"], "second_user": data["second_user"]}
+            insert_data["message"] = [{data["message"]: "from_first"}]
+            collection_name.insert_one(insert_data)
 
-    collection_name.insert_one(data)
-    return {"haha"}
+    return {"message": "Message sent!"}
 
 
