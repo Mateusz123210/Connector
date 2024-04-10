40d6d91acd2d661e5b5979d0bc61f8fb24bc17cc
Mateusz123210 <mateusz.urbanczyk11@gmail.com>
2024-04-10 21:31:47 +0200

    Working server on localhost

--- app/main.py
+++ app/main.py
@@ -12,15 +12,14 @@ app = FastAPI()
 async def send_message(data: MessageSending = Depends(validate_token_for_message_sending)):
     return services.send_message(dict(data))
 
-@app.get("/get-messages")
+@app.post("/get-messages")
 async def get_messages(data: MessagesFetching = Depends(validate_token_for_message_fetching)):
     return services.get_messages(dict(data))
 
-@app.delete("/delete-messages")
+@app.post("/delete-messages")
 async def delete_messages(data: MessagesDeleting = Depends(validate_token_for_messages_deleting)):
     return services.delete_messages(dict(data))
 
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
-    
