1ba2f34667606b66a3f7a028138c46ffbcc17989
Mateusz123210 <mateusz.urbanczyk11@gmail.com>
2024-04-27 22:17:28 +0200

    Added ssl

--- app/main.py
+++ app/main.py
@@ -6,11 +6,12 @@ from schemas import MessageSending, MessagesDeleting, MessagesFetching
 from deps import validate_token_for_message_fetching, validate_token_for_message_sending, \
     validate_token_for_messages_deleting
 from fastapi.middleware.cors import CORSMiddleware
+import ssl
 
 app = FastAPI()
 
 origins = [
-    "*"
+    "http://localhost:8000"
 ]
 
 app.add_middleware(
@@ -33,6 +34,12 @@ async def get_messages(data: MessagesFetching = Depends(validate_token_for_messa
 async def delete_messages(data: MessagesDeleting = Depends(validate_token_for_messages_deleting)):
     return services.delete_messages(dict(data))
 
+ssl_certfile = './app/ssl_certificate/cert.cer'
+ssl_keyfile = './app/ssl_certificate/key.pem'
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain(certfile=ssl_certfile, keyfile=ssl_keyfile)
+
 
 if __name__ == '__main__':
-    uvicorn.run(app, port=8080, host='0.0.0.0')
+    uvicorn.run(app, port=8080, host='0.0.0.0', ssl_certfile=ssl_certfile, ssl_keyfile=ssl_keyfile)
